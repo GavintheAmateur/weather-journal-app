3387532894bf2abc2998ce3e64bb6eab5751a601
GavintheAmateur <gavin.neolife@gmail.com>
2021-02-17 17:42:37 +0800

    add .gitignore

--- server.js
+++ server.js
@@ -1,19 +1,45 @@
 // Setup empty JS object to act as endpoint for all routes
-projectData = {};
+let projectData = {};
 
 // Require Express to run server and routes
-
+const express = require('express');
 // Start up an instance of app
-
+const app = express();
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
+const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
-
+const cors = require('cors');
+app.use(cors());
 // Initialize the main project folder
 app.use(express.static('website'));
 
 
+
+
 // Setup Server
+const port = 5555;
+// Callback to debug
+const listening = () => {
+    console.log("hello, I received your message!");
+}
+const server = app.listen(port, listening);
+
+
+
+// Get Route
+const getJournalHandler = (req,resp)=> {
+    console.log(req)
+    resp.send("hello world");
+}
+
+app.get('/all',getJournalHandler);
+// Post Route
+const postJournalHandler = ()=> {
+
+}
+
+app.post('/',postJournalHandler);
--- website/app.js
+++ website/app.js
@@ -1,5 +1,98 @@
 /* Global Variables */
 
-// Create a new date instance dynamically with JS
-let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let weatherAPIAppid = '0b78a979660e620d1b8ad5f422f0c144';
+let weatherAPIUrl =  new URL("https://api.openweathermap.org/data/2.5/weather");
+
+/*
+End global variables
+Begin helper functions*/
+
+const logAPICallError = err => console.warn(`ERROR ${err.code}: ${err.message}`);
+
+/* Function to GET data */
+
+const callAPI = async (url = '',method='GET',headers=headers,onSuccess=null,onFailure=null) => {
+    try {
+        const resp = await fetch(url, {
+            method: method,
+            credentials: '',
+            headers: headers
+        })
+        onSuccess(resp);
+    } catch (error) {
+        logAPICallError(error);
+        onFailure();
+    }
+
+}
+
+/* Function to POST data */
+const postData = async (url = '', data = {}) => {
+    console.log(data)
+    const response = await fetch(url, {
+        method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        credentials: 'same-origin', // include, *same-origin, omit
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data), // body data type must match "Content-Type" header        
+    });
+
+    try {
+        const newData = await response.json();
+        // console.log(newData);
+        return newData
+    } catch (error) {
+        console.log("error", error);
+        // appropriately handle the error
+    }
+}
+
+
+/*
+End helper functions.
+Begin business logic:
+*/
+
+//1.fetch weather when page opens
+//1.1 get current location
+
+const renderWeatherFromResponse = resp => {
+    let data = resp.body
+    console.log("data retrieved in renderWeatherFromResponse"+data);
+}
+const onGetGeoSuccess = pos=> {
+    const lat = pos.coords.latitude;
+    const lon = pos.coords.longgitude;
+    params = {
+        lat:lat,
+        lon:lon,
+        cnt:1
+    }
+    Object.keys(params).forEach(key=> weatherAPIUrl.searchParams.append(key,params[key]));
+    callAPI(weatherAPIUrl,
+        'GET',
+        {
+            'Content-Type': 'application/json',
+        },
+        renderWeatherFromResponse
+        );
+}
+
+navigator.geolocation.getCurrentPosition(
+    onGetGeoSuccess,
+    logAPICallError,
+    {
+        enableHighAccuracy: true,
+        timeout: 1000,
+        maximumAge: 0      
+    });
+//2. fetch weather when manually input a zip
+
+//3. save journal when clicking Generate button
+
+const saveJournal = () => {
+
+}
+const generateBtn = document.getElementById('generate');
+generateBtn.addEventListener('click', saveJournal);
